8667e6c1697bc07551aa6deae5743a3f5ad3c1a9
dmitri <deemok@gmail.com>
2021-01-20 13:02:51 +0100

    Address review comments

--- lib/client/keyagent_test.go
+++ lib/client/keyagent_test.go
@@ -38,6 +38,7 @@ import (
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/sshutils"
 	"github.com/gravitational/teleport/lib/tlsca"
+	"github.com/gravitational/teleport/lib/utils"
 
 	"github.com/gravitational/trace"
 
@@ -461,9 +462,7 @@ func (s *KeyAgentTestSuite) makeKey(username string, allowedLogins []string, ttl
 }
 
 func startDebugAgent() (closer func(), err error) {
-	startedC := make(chan struct{})
 	rand.Seed(time.Now().Unix())
-
 	socketpath := filepath.Join(os.TempDir(),
 		fmt.Sprintf("teleport-%d-%d.socket", os.Getpid(), rand.Uint32()))
 
@@ -475,6 +474,7 @@ func startDebugAgent() (closer func(), err error) {
 	systemAgent := agent.NewKeyring()
 	os.Setenv(teleport.SSHAuthSock, socketpath)
 
+	startedC := make(chan struct{})
 	doneC := make(chan struct{})
 	var wg sync.WaitGroup
 	wg.Add(2)
